d748b186ac0542f302f049547d822b326ac72f00
Peter Elliott <pelliott@ualberta.ca>
2019-09-09 15:13:41 -0600

    added test/util.scm

---
+++ test/util.scm
@@ -0,0 +1,14 @@
+(use-modules (guest test)
+             (schingle util))
+
+(define-test (schingle util multi-map)
+  (equal? (multi-map '()) '())
+  (equal? (multi-map '(1 2 3)) '(1 2 3))
+  (equal? (multi-map '() 1+ 1+) '())
+  (equal? (multi-map '(1 2 3) 1+ 1+) '(3 4 5)))
+
+(define-test (schingle util intersperse)
+  (equal? (intersperse '* '()) '())
+  (equal? (intersperse '* '(a)) '(a))
+  (equal? (intersperse '* '(a b c)) '(a * b * c))
+  (equal? (intersperse '* '(a b c d)) '(a * b * c * d)))
