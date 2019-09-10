dc426c33cbad34f9e8dad2595dcf03013f803794
Peter Elliott <pelliott@ualberta.ca>
2019-09-10 09:21:07 -0600

    added route pathmap tests

---
+++ test/route.scm
@@ -0,0 +1,18 @@
+(use-modules (guest test)
+             (schingle route))
+
+(define make-pathmap (@@ (schingle route) make-pathmap))
+(define pathmap-ref (@@ (schingle route) pathmap-ref))
+
+(define-test (schingle route pathmap)
+  (equal? (pathmap-ref #f '("a" "b") 'default) 'default)
+  (equal? (pathmap-ref (make-hash-table) '("a" "b") 'default) 'default)
+  (equal? (pathmap-ref (make-pathmap '("a" :b) 5) '("a" "b") 'default) 5)
+  (equal? (pathmap-ref (make-pathmap '("a" "c") 5) '("a" "b") 'default) 'default)
+  (equal? (pathmap-ref (make-pathmap '("a" "c") 5) '("a" "c" "d")) #f))
+
+(define-test (schingle route pathmap-splat)
+  (equal? (pathmap-ref (make-pathmap '("a" (*) "b") 5) '("a" "b")) 5)
+  (equal? (pathmap-ref (make-pathmap '("a" (*) "b") 5) '("a" "c" "b")) 5)
+  (equal? (pathmap-ref (make-pathmap '("a" (*) "b") 5) '("a" "c" "d" "b")) 5)
+  (equal? (pathmap-ref (make-pathmap '("a" (*) "b") 5) '("a" "c" "d" "b")) 5))
