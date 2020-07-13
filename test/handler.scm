e00a7f0a3d14e6c74dec12e572059347aa87a05c
Peter Elliott <pelliott@ualberta.ca>
2020-07-12 18:23:42 -0600

    updated tests for new version of guest

--- test/handler.scm
+++ test/handler.scm
@@ -12,31 +12,31 @@
     (equal? va vb)))
 
 (define-test (schingle handler 400handler)
-  (values-equal?
-    (values
-      (build-response
-        #:code 400
-        #:headers '((content-type . (text/plain))))
-      "400 Bad Request")
-    ((400handler) #f #f)))
+  (assert (values-equal?
+           (values
+            (build-response
+             #:code 400
+             #:headers '((content-type . (text/plain))))
+            "400 Bad Request")
+           ((400handler) #f #f))))
 
 (define-test (schingle handler 404handler)
-  (values-equal?
-    (values
-      (build-response
-        #:code 404
-        #:headers '((content-type . (text/plain))))
-      "404 Not Found")
-    ((404handler) #f #f)))
+  (assert (values-equal?
+           (values
+            (build-response
+             #:code 404
+             #:headers '((content-type . (text/plain))))
+            "404 Not Found")
+           ((404handler) #f #f))))
 
 (define-test (schingle handler 500handler)
-  (values-equal?
-    (values
-      (build-response
-        #:code 500
-        #:headers '((content-type . (text/plain))))
-      "500 Internal Server Error")
-    ((500handler) #f #f)))
+  (assert (values-equal?
+           (values
+            (build-response
+             #:code 500
+             #:headers '((content-type . (text/plain))))
+            "500 Internal Server Error")
+           ((500handler) #f #f))))
 
 (define (test-handler handler)
   (routes->handler
@@ -44,18 +44,18 @@
       handler)))
 
 (define-test (schingle handler routes->handler)
-  (values-equal?
-    ((test-handler #f)
-     (build-request (string->uri "http://localhost"))
-     #f)
-    ((404handler) #f #f))
-  (values-equal?
-    ((test-handler (cons '() (lambda (p r b) (car '()))))
-     (build-request (string->uri "http://localhost"))
-     #f)
-    ((500handler) #f #f))
-  (values-equal?
-    ((test-handler (cons '() (lambda (p r b) (values 1 2))))
-     (build-request (string->uri "http://localhost"))
-     #f)
-    (values 1 2)))
+  (assert (values-equal?
+           ((test-handler #f)
+            (build-request (string->uri "http://localhost"))
+            #f)
+           ((404handler) #f #f)))
+  (assert (values-equal?
+           ((test-handler (cons '() (lambda (p r b) (car '()))))
+            (build-request (string->uri "http://localhost"))
+            #f)
+           ((500handler) #f #f)))
+  (assert (values-equal?
+           ((test-handler (cons '() (lambda (p r b) (values 1 2))))
+            (build-request (string->uri "http://localhost"))
+            #f)
+           (values 1 2))))
--- test/static.scm
+++ test/static.scm
@@ -25,66 +25,68 @@
 
 (define-test (schingle static static)
   (with-debug-fns
-    (values-equal?
-      (values
-        (build-response
-          #:code 200
-          #:headers '((content-type . (application/json))))
-        (string->bytevector "{\"a\": 5}" "utf-8"))
-      (static "a.json" 'application/json)))
+    (assert (values-equal?
+             (values
+              (build-response
+               #:code 200
+               #:headers '((content-type . (application/json))))
+              (string->bytevector "{\"a\": 5}" "utf-8"))
+             (static "a.json" 'application/json))))
   (with-debug-fns
-    (values-equal?
-      (values
-        (build-response
-          #:code 200
-          #:headers '((content-type . (application/json))))
-        (string->bytevector "{\"a\": 5}" "utf-8"))
-      (static "a.json" 'application/json)))
+    (assert (values-equal?
+             (values
+              (build-response
+               #:code 200
+               #:headers '((content-type . (application/json))))
+              (string->bytevector "{\"a\": 5}" "utf-8"))
+             (static "a.json" 'application/json))))
   (with-debug-fns
-    (values-equal?
-      (values
-        (build-response
-          #:code 200
-          #:headers '((content-type . (application/xml))))
-        (string->bytevector "<a>5</a>" "utf-8"))
-      (static "a.xml" 'application/xml)))
+    (assert (values-equal?
+             (values
+              (build-response
+               #:code 200
+               #:headers '((content-type . (application/xml))))
+              (string->bytevector "<a>5</a>" "utf-8"))
+             (static "a.xml" 'application/xml))))
   (with-debug-fns
-    (values-equal?
-      (values
-        (build-response
-          #:code 200
-          #:headers '((content-type . (application/xml))))
-        (string->bytevector "<a>5</a>" "utf-8"))
-      (static "a.xml" 'application/xml)))
+    (assert (values-equal?
+             (values
+              (build-response
+               #:code 200
+               #:headers '((content-type . (application/xml))))
+              (string->bytevector "<a>5</a>" "utf-8"))
+             (static "a.xml" 'application/xml))))
   (with-debug-fns
-    (values-equal?
-      (values
-        (build-response
-          #:code 404)
-          "404 Not Found")
-      (static "a.x" 'application/xml))))
+   (assert (values-equal?
+            (values
+             (build-response
+              #:code 404
+              #:headers '((content-type . (text/plain))))
+             "404 Not Found")
+            (static "a.x" 'application/xml)))))
 
 (define-test (schingle static static-guess-content)
   (with-debug-fns
-    (values-equal?
-      (values
-        (build-response
-          #:code 200
-          #:headers '((content-type . (application/json))))
-        (string->bytevector "{\"a\": 5}" "utf-8"))
-      (static "a.json")))
+    (assert (values-equal?
+             (values
+              (build-response
+               #:code 200
+               #:headers '((content-type . (application/json))))
+              (string->bytevector "{\"a\": 5}" "utf-8"))
+             (static "a.json"))))
   (with-debug-fns
-    (values-equal?
-      (values
-        (build-response
-          #:code 200
-          #:headers '((content-type . (application/xml))))
-        (string->bytevector "<a>5</a>" "utf-8"))
-      (static "a.xml")))
+    (assert (values-equal?
+             (values
+              (build-response
+               #:code 200
+               #:headers '((content-type . (application/xml))))
+              (string->bytevector "<a>5</a>" "utf-8"))
+             (static "a.xml"))))
   (with-debug-fns
-    (values-equal?
-      (values
-        (build-response
-          #:code 404)
-          "404 Not Found")
-      (static "a.x"))))
+    (assert (values-equal?
+             (values
+              (build-response
+               #:code 404
+               #:headers '((content-type . (text/plain))))
+              "404 Not Found")
+             (static "a.x")))))
