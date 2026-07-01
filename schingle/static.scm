ec0472f15a6e659e43deab58f3e6f8b53919b4e0
Peter Elliott <p.elliott332@gmail.com>
2026-06-30 22:53:46 -0600

    ftypes: move file type logic out of static into ftypes

--- schingle/static.scm
+++ schingle/static.scm
@@ -1,5 +1,4 @@
 (define-module (schingle static)
-  #:use-module (ice-9 regex)
   #:use-module (web response)
   #:use-module (schingle ftypes)
   #:use-module (schingle status)
@@ -7,7 +6,7 @@
   #:export (static
             file-content-type))
 
-(define* (static file #:optional (content-type (file-content-type file))
+(define* (static file #:optional (content-type (extension->content-type file))
                  #:key (transf identity))
   "load and serve a static file from the cache with content-type content-type.\
   if content-type is not provided, it will be determined from the file extention"
@@ -19,9 +18,3 @@
         #:headers `((content-type . (,content-type))))
        data)
       (not-found)))
-
-(define ext-reg (make-regexp "\\.[^\\.]*$"))
-
-(define (file-content-type file)
-  "determines the content-type of a file from extension to mime type"
-  (hash-ref extension-table (match:substring (regexp-exec ext-reg file))))
